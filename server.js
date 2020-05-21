e7905f90eacff4258829b0f0625ee35e3ef96922
park129693 <park129693@gmail.com>
2020-05-21 17:13:30 +0900

    ejs 등록

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express')
-
+var path = require('path')
+var bodyParser = require('body-parser')
 var app = express()
+
+
+app.set('views', path.join(__dirname+'/views')) /*초기화 __dirname: 현재 디렉터리까지의 경로를 알려줌*/
+app.set('view engine', 'ejs')/*view에 ejs 확장자 사용*/
+
+// app.use()
 
 app.get('/',function(request, response){
     console.log(request)
-    response.send("<h1 style='color:green'>안녕하세요</h1>")
+    response.render('index')
 })
 
 // listne(포트, url, backlog, callback)
